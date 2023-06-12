03c59c813eb2343e7c5ea188d22cca86f4b0a4cc
lcspaiva <lcspaiva87@gmail.com>
2023-06-12 15:20:17 -0300

    create router aplicaçao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'createThought',
+    component: CreateThoughtComponent,
+  },
+  {
+    path: 'listThought',
+    component: ListThoughtComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'listThought',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
